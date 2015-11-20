6a4d0e5fefddb0b90ed351f251049bb0c58b9859
samoooop <samoooop@DESKTOP-886QIJ2>
2015-11-21 03:44:22 +0700

    edit MovingObject -> movingObject

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -2,61 +2,56 @@ import java.util.ArrayList;
 
 import render.IRenderable;
 
-
 public class GameLogic {
-private static GameLogic instance =  new GameLogic();
-protected ArrayList<MovingObject> MovingObject;
-private static final  int SPAWN_DELAY = 100;
-private int spawnDelayCounter;
-public GameLogic(){
-	
-	MovingObject = new ArrayList<MovingObject>();
-	spawnDelayCounter  =0;
-}
-public static GameLogic getInstance() {
-	return instance;
-}
-public void logicUpdate(){
-	for(int i = 0 ;i <MovingObject.size();i++){
-		MovingObject.get(i).update();
-		for(int j = 0 ;j <MovingObject.size();j++ ){
-			if(MovingObject.get(i).collideWith(MovingObject.get(j))){
-				MovingObject.get(i).setDestroyed(true);
-			}
-		}
-	   if(MovingObject.get(i).isDestroyed() ){
-		   MovingObject.remove(i);
-		   i--;
-	   }
-	   
-	
-	
-	
-	
-	
+	private static GameLogic instance = new GameLogic();
+	protected ArrayList<MovingObject> movingObject;
+	private static final int SPAWN_DELAY = 100;
+	private int spawnDelayCounter;
+
+	public GameLogic() {
+
+		movingObject = new ArrayList<MovingObject>();
+		spawnDelayCounter = 0;
 	}
-	
-	ArrayList <IRenderable> temp = RenderableHolder.getInstance().getEntities();
-	for (int i = 0; i < temp.size(); i++) {
-		if (temp.get(i) instanceof MovingObject) {
-			if (((MovingObject) temp.get(i)).isDestroyed()) {
-				temp.remove(i);
+
+	public static GameLogic getInstance() {
+		return instance;
+	}
+
+	public void logicUpdate() {
+		for (int i = 0; i < movingObject.size(); i++) {
+			movingObject.get(i).update();
+			for (int j = 0; j < movingObject.size(); j++) {
+				if (movingObject.get(i).collideWith(movingObject.get(j))) {
+					movingObject.get(i).setDestroyed(true);
+				}
+			}
+			if (movingObject.get(i).isDestroyed()) {
+				movingObject.remove(i);
 				i--;
 			}
+
 		}
+
+		ArrayList<IRenderable> temp = RenderableHolder.getInstance().getEntities();
+		for (int i = 0; i < temp.size(); i++) {
+			if (temp.get(i) instanceof MovingObject) {
+				if (((MovingObject) temp.get(i)).isDestroyed()) {
+					temp.remove(i);
+					i--;
+				}
+			}
+		}
+
+		spawnDelayCounter++;
+		if (spawnDelayCounter >= SPAWN_DELAY) {
+			spawnDelayCounter = 0;
+			MovingObject a = new MovingObject();
+			movingObject.add(a);
+			RenderableHolder.getInstance().getEntities().add(a);
+
+		}
+
 	}
-
-	spawnDelayCounter ++;
-	if(spawnDelayCounter >= SPAWN_DELAY){
-		spawnDelayCounter = 0;
-		MovingObject a = new MovingObject();
-		MovingObject.add(a);
-		RenderableHolder.getInstance().getEntities().add(a);
-		
-	}
-	
-}
-
-
 
 }
