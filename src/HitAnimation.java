a59414002d6df34b855592374f2a648584a6a699
Trairat <samoooop@gmail.com>
2015-12-12 12:03:06 +0700

    123

--- src/HitAnimation.java
+++ src/HitAnimation.java
@@ -11,7 +11,7 @@ public class HitAnimation implements IRenderable, Destroyable {
 	private static int CIRCLE_THICK = 2;
 	private static int MAX_CHAIN_TIME = 3;
 	private static int CHAIN_DELAY_TIME = 10;
-	private static boolean CAN_CHAIN = true; // still buggy(concurrent)
+	private static boolean CAN_CHAIN = false; // still buggy(concurrent)
 	private static int ANIMATE_SPEED = 2;
 	private int time;
 	private int radius;
