96d845686564c13099e719f3e255903fa7971d57
Trairat <samoooop@gmail.com>
2015-12-14 14:28:34 +0700

    add new way to load audio(not used yet)

--- src/util/AudioUtility.java
+++ src/util/AudioUtility.java
@@ -2,6 +2,11 @@ package util;
 
 import java.applet.Applet;
 import java.applet.AudioClip;
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class AudioUtility {
 	public static final AudioClip bgm = loadAudio("res/sound/bgm.wav");
@@ -10,6 +15,7 @@ public class AudioUtility {
 	public static final AudioClip crashSound = loadAudio("res/sound/crash.wav");
 	public static final AudioClip solvedSound = loadAudio("res/sound/solved.wav");
 	public static final AudioClip soundtrackBG = loadAudio("res/sound/xeno.wav");
+	public static final Clip soundTrackBG = loadClip("res/sound/xeno.wav");
 	private static boolean isMuted;
 
 	private static AudioClip loadAudio(String directory) {
@@ -21,6 +27,21 @@ public class AudioUtility {
 			a = null;
 		}
 		return a;
+	}
+	
+	private static Clip loadClip(String directory) {
+		Clip c;
+		try {
+			c = AudioSystem.getClip();
+			ClassLoader load = DrawingUtility.class.getClassLoader();
+			//a = Applet.newClip((load.getResource(directory)).getFile());
+			AudioInputStream ais = (AudioSystem.getAudioInputStream(load.getResource(directory)));
+			c.open(ais);
+			c.loop(1);
+		} catch (Exception e) {
+			c = null;
+		}
+		return c;
 	}
 
 	public static void setMuted(boolean isMuted) {
