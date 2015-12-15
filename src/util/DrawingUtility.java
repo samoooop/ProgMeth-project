2795137207ad2d13c88a1e15c49ea664701a0710
HAMMOSTEX <HAMMOSTEX@DELL>
2015-12-16 05:01:37 +0700

    Version2

--- src/util/DrawingUtility.java
+++ src/util/DrawingUtility.java
@@ -126,12 +126,8 @@ public class DrawingUtility {
 	public static void setGameScreen(GameScreen gs) {
 		gameScreen = gs;
 	}
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'TEST_inputUtility' of https://github.com/samoooop/ProgMeth-project.git
 	
-<<<<<<< HEAD
 	public static double getWidthResizeFactor() {
 		if (gameScreen == null) {
 			return 1;
@@ -148,8 +144,6 @@ public class DrawingUtility {
 		}
 	}
 
-=======
->>>>>>> branch 'TEST_inputUtility' of https://github.com/samoooop/ProgMeth-project.git
 	public static void drawCircle(Graphics2D g2, int x, int y, int innerRadius, int outterRadius, Color innerColor,
 			Color outterColor) {
 		g2.setColor(outterColor);
